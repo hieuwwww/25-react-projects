21d34ec36fda47af2053ab358ea45f577dcaac31
hieuwwww <wibutrua@gmail.com>
2024-04-20 22:51:29 +0700

    d3p3

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color-generator";
 
 function App() {
   return (
     <div className="App">
-      {/* Accordian component*/}
+      {/* Accordian component
       <Accordian />
-      {/* {RandomColor component} */}
+      {RandomColor component}
       <RandomColor />
+      Image slider coponent */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
